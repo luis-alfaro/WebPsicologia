52f9ecf49f181951ec48870c20d3d9229a15f733
luis-alfaro <lead123456@hotmail.com>
2020-07-10 13:29:13 -0500

    Definir arquitectura básica del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BienvenidaComponent } from './modules/public/bienvenida/bienvenida.component';
+import { PacienteListComponent } from './modules/pacientes/paciente-list/paciente-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, BienvenidaComponent, PacienteListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import './polyfills';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environments/environment';
 
 import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
