94f3581f7ac0f3c8350773c03d5f64db8c90c6f8
luis-alfaro <lead123456@hotmail.com>
2020-07-23 00:00:36 -0500

    Crear servicios en core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { PacienteService } from './paciente.service';
 
 @NgModule({
-  imports: [ ]
+  imports: [ ],
+  providers: [PacienteService]
 })
 export class CoreModule { }
