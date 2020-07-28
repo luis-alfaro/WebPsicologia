6d0ca010de32712b838a5dc61a8fdb8bdaa15f17
luis-alfaro <lead123456@hotmail.com>
2020-07-28 00:53:53 -0500

    Crear mock template de paciente

---
+++ src/app/core/mocks/paciente-template.mock.ts
@@ -0,0 +1,12 @@
+import { Paciente } from "../../shared/models/paciente";
+
+export const PACIENTE: Paciente = {
+  _id: "5efa7f4cb9515005b0bd5cc{id}",
+  nombres: "Nombres Paciente{id}",
+  apellidos: "Apellidos Paciente{id}",
+  telefono: "Telefono Paciente{id}",
+  celular: "Celular Paciente{id}",
+  email: "email paciente{id}",
+  descripcion: "Descripcion Paciente{id}",
+  psicologoId: null
+}
--- src/app/core/services/paciente.service.ts
+++ src/app/core/services/paciente.service.ts
@@ -3,12 +3,36 @@ import { Observable, of } from 'rxjs';
 
 import { Paciente } from '../../shared/models/paciente';
 import { PACIENTES } from '../mocks/paciente.mock';
+import { PACIENTE } from '../mocks/paciente-template.mock';
 
 @Injectable()
 export class PacienteService {
-  constructor() { }
+  listPaciente: Paciente[] = [];
+
+  constructor() {
+    for (let i: number = 0; i < 5; i++) this.listPaciente.push(this.next());
+  }
 
   getAll(): Observable<Paciente[]> {
-    return of(PACIENTES);
+    return of(this.listPaciente);
   }
+
+  create(paciente: Paciente): void {
+    this.listPaciente.push(paciente);
+  }
+
+  new(id: string): Paciente {
+    return {
+      _id: PACIENTE._id.replace('{id}', id),
+      nombres: PACIENTE.nombres.replace('{id}', id),
+      apellidos: PACIENTE.apellidos.replace('{id}', id),
+      telefono: PACIENTE.telefono.replace('{id}', id),
+      celular: PACIENTE.celular.replace('{id}', id),
+      email: PACIENTE.email.replace('{id}', id),
+      descripcion: PACIENTE.descripcion.replace('{id}', id),
+      psicologoId: null
+    };
+  }
+
+  next(): Paciente { return this.new(this.listPaciente.length.toString()); }
 }
