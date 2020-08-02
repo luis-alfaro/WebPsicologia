b5edbff016e290a1d6a26dbd43547b5018cfffe4
luis-alfaro <lead123456@hotmail.com>
2020-08-01 22:04:27 -0500

    Crear componente shared table

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   exports: [
     CommonModule,
     FormsModule
-  ]
+  ],
+  declarations: [TableComponent]
 })
 export class SharedModule { }
