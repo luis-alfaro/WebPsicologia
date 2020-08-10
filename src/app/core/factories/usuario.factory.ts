1d3468032dd47c048def6d89998cab9f9d2b3f51
luis-alfaro <lead123456@hotmail.com>
2020-08-10 17:00:27 -0500

    Se agrega lectura de variables de environment para hacer comparación en deploy con config.json

--- src/app/core/factories/usuario.factory.ts
+++ src/app/core/factories/usuario.factory.ts
@@ -1,8 +1,10 @@
+import { environment } from '../../../environments/environment';
 import config from '../../../assets/config.json';
 import { UsuarioServiceI } from '../servicesI/usuario.serviceI';
 import { UsuarioMockService } from '../servicesMock/usuario-mock.service';
 
 export const UsuarioFactory = (): UsuarioServiceI => {
+  console.log(environment);
   console.log(config);
   return new UsuarioMockService();
 };
