89654a6a00e001a5b0b942892dc0363faf87c830
luis-alfaro <lead123456@hotmail.com>
2020-08-17 18:16:35 -0500

    Ajuste de error en server.js (faltaba importar express).

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const express = require('express');
+
+const app = express();
 app.use(express.static('./dist/lead-first-app'));
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/lead-first-app/'});
